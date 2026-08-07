2e4d31d9f18a494a2361518dc04159e9516d6d58
ibnhosain <ibnhosain014@gmail.com>
2026-08-07 20:05:51 +0600

    add day 02 camplate

--- Day_02/Datatypes.js
+++ Day_02/Datatypes.js
@@ -26,3 +26,61 @@ document.getElementById("bigintOutput").innerHTML = starsInGalaxy ;
 document.getElementById("bigintType").innerHTML = typeof starsInGalaxy;
 
 
+//Boolean
+
+let x = 40;
+let y = 50;
+let z = x < y;
+
+document.getElementById("booleanName").innerHTML = "Boolean";
+document.getElementById("booleanOutput").innerHTML = z;
+document.getElementById("booleanType").innerHTML = typeof z;
+
+let a = 40;
+let b = 50;
+let c = b < a;
+
+document.getElementById("booleanfName").innerHTML = "Boolean";
+document.getElementById("booleanfOutput").innerHTML = c;
+document.getElementById("booleanfType").innerHTML = typeof c;
+
+//Object
+
+let book = {
+ Name: "Learn Javascript",
+ Author : "Arif Mahmud",
+ Publisher: "Green mart"
+}
+
+document.getElementById("objectName").innerHTML = "Object";
+document.getElementById("objectOutput").innerHTML =
+book.Name + "<br>" +
+book.Author + "<br>" +
+book.Publisher;
+
+document.getElementById("objectType").innerHTML = typeof book;
+
+//Undefined
+let v ;
+
+document.getElementById("undefinedName").innerHTML = "Undefined";
+document.getElementById("undefinedOutput").innerHTML = v;
+document.getElementById("undefinedType").innerHTML = typeof v;
+
+
+
+//Null
+let flower = null;
+
+document.getElementById("nullName").innerHTML = "Null";
+document.getElementById("nullOutput").innerHTML = String(flower);
+document.getElementById("nullType").innerHTML = typeof flower;
+
+
+//Symbol
+
+let nid = Symbol("id");
+
+document.getElementById("symbolName").innerHTML = "Symbol";
+document.getElementById("symbolOutput").innerHTML = String(nid);
+document.getElementById("symbolType").innerHTML = typeof nid;
